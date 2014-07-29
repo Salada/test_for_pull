7d349d95d8e238d3f01e06ba7f146875ced51996
Kim Young Jun <kyjbusan@gmail.com>
2014-07-29 21:22:01 +0900

    teest redmine github web hook

--- main.c
+++ main.c
@@ -5,25 +5,5 @@ int main(){
 	
 	// Dev branch
 	
-	// Update 2
-
-	// Update 3
-	
-	// Update 4
-	
-	// Update 5
-
-	// Update 6
-	
-	// Update 7
-	
-	// Update 8
-	
-	// Update 9
-	
-	// Update 10
-	
-	// Update 11
-	
 	return 0;
 }
